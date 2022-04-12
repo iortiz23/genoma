fff7936fe54604469316798709bab53bad8be18f
Esneider Rocha Torres <neider.1991@gmail.com>
2022-04-11 19:59:58 -0500

    ajuste lenguaje de la tabla

--- pages/load/load.php
+++ pages/load/load.php
@@ -255,6 +255,23 @@ $IdProfile = $_SESSION['IdProfile'];
             });
         });
         $("#table_Loads").DataTable({
+            "oLanguage": {
+                "sProcessing": "Procesando...",
+                "sLengthMenu": "Mostrar _MENU_ registros",
+                "sZeroRecords": "No se encontraron resultados",
+                "sInfo": "Mostrando desde _START_ hasta _END_ de _TOTAL_ registros",
+                "sInfoEmpty": "Mostrando desde 0 hasta 0 de 0 registros",
+                "sInfoFiltered": "(filtrado de _MAX_ registros en total)",
+                "sInfoPostFix": "",
+                "sSearch": "Buscar:",
+                "sUrl": "",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sPrevious": "Anterior",
+                    "sNext": "Siguiente",
+                    "sLast": "Ultimo"
+                }
+            },
             "responsive": true,
             "lengthChange": false,
             "autoWidth": false,
